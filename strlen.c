d06b6475ce79fe493e91d915548948a5c9dc451d
Gaurav-r-93 <92088535+Gaurav-r-93@users.noreply.github.com>
2021-10-13 16:50:19 +0530

    Create strlen.c

---
+++ strlen.c
@@ -0,0 +1,9 @@
+#include <string.h>
+#include <stdio.h>
+
+int main(void) 
+{
+  char name[50] = "R N S Institute of technology";
+  int length = strlen(name);
+  printf("Name length: %u", length);
+}
